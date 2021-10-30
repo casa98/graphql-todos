ade4e33d2f893b60a3c7eb3fb58c8d766aaf94a1
Carlos Andrés Sánchez <carlos.sanchez@safekeepingapp.com>
2021-10-30 08:34:19 -0500

    Fixes to support creating and uploading new ToDos

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,34 +13,48 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Private ToDos'),
       ),
-      body: Query(
-        options: QueryOptions(
-          document: gql(QueryStrings.getPrivateTodos),
-          variables: {"is_public": false},
-          //pollInterval: const Duration(seconds: 10),
-        ),
-        builder: (QueryResult result,
-            {VoidCallback? refetch, FetchMore? fetchMore}) {
-          if (result.hasException) {
-            return Center(child: Text(result.exception.toString()));
-          }
+      body: Column(
+        children: [
+          //TODO: Add TextField to create new ToDos
+          Query(
+            options: QueryOptions(
+              document: gql(QueryStrings.getPrivateTodos),
+              variables: {"is_public": false},
+              //pollInterval: const Duration(seconds: 10),
+            ),
+            builder: (QueryResult result,
+                {VoidCallback? refetch, FetchMore? fetchMore}) {
+              if (result.hasException) {
+                return Expanded(
+                  child: Center(child: Text(result.exception.toString())),
+                );
+              }
 
-          if (result.isLoading) return const Center(child: Text('Loading...'));
+              if (result.isLoading) {
+                return const Expanded(
+                  child: Center(child: Text('Loading...')),
+                );
+              }
 
-          final List<TodoItem> todos = (result.data?['todos'] as List<dynamic>)
-              .map((e) => TodoItem.fromElements(e))
-              .toList();
-          return ListView.builder(
-            itemCount: todos.length,
-            itemBuilder: (context, index) {
-              TodoItem todo = todos[index];
-              return ListTile(
-                title: Text(todo.title),
-                subtitle: Text('Is Completed: ${todo.isCompleted}'),
+              final List<TodoItem> todos =
+                  (result.data?['todos'] as List<dynamic>)
+                      .map((e) => TodoItem.fromElements(e))
+                      .toList();
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    TodoItem todo = todos[index];
+                    return ListTile(
+                      title: Text(todo.title),
+                      subtitle: Text('Is Completed: ${todo.isCompleted}'),
+                    );
+                  },
+                ),
               );
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
